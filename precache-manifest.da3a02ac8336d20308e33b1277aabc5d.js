4ab020fbe54e141ed6380b2a0cee0e927ccee43c
suprgyabhushan <Suprgya.56@iiitb.org>
2021-08-02 21:20:49 +0530

    Updates

---
+++ precache-manifest.da3a02ac8336d20308e33b1277aabc5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85748c505030ecc0f5a583ca02398f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9454ba988033ac6faf60",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9454ba988033ac6faf60",
+    "url": "/static/js/main.ed9520c8.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "f8d5d2c5549c2281bedc9150cd71e67e",
+    "url": "/static/media/resume.f8d5d2c5.pdf"
+  }
+]);
